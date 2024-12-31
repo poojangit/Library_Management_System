a33d4bb09060681954ccdacd76454774991af4c4
poojangit <poojang147@gmail.com>
2024-12-31 14:35:58 +0530

    [POOJA] modified : Encapsulation - Implemented the principles of encapsulation in books class

--- src/books/Book.java
+++ src/books/Book.java
@@ -1,8 +1,33 @@
 package books;
 
 public class Book {
-    String title;
-    String author;
-    String isbn;
-    boolean isCheckedOut;
+   private String title;
+   private String author;
+   private String isbn;
+   private boolean isCheckedOut;
+
+   public void setTitle(String title){
+    this.title = title;
+   }
+   public String getTitle(){
+    return title;
+   }
+   public void setAuthor(String author){
+    this.author = author;
+   }
+   public String getAuthor() {
+    return author;
+   }
+   public void setIsbn(String isbn){
+    this.isbn = isbn;
+   }
+   public String getIsbn() {
+    return isbn;
+   } 
+   public void setIsCheckedOut(boolean isCheckedOut){
+    this.isCheckedOut = isCheckedOut;
+   }
+   public boolean getIsCheckedOut() {
+    return isCheckedOut;
+   }
 }
