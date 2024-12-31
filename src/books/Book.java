c70584896272cf644cdf26dd15ab329f735ce159
poojangit <poojang147@gmail.com>
2024-12-31 14:39:37 +0530

    [POOJA] Added : Applied encapsulation principles for book class

--- src/books/Book.java
+++ src/books/Book.java
@@ -1,33 +1,40 @@
 package books;
 
 public class Book {
-   private String title;
-   private String author;
-   private String isbn;
-   private boolean isCheckedOut;
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isCheckedOut;
 
-   public void setTitle(String title){
-    this.title = title;
-   }
-   public String getTitle(){
-    return title;
-   }
-   public void setAuthor(String author){
-    this.author = author;
-   }
-   public String getAuthor() {
-    return author;
-   }
-   public void setIsbn(String isbn){
-    this.isbn = isbn;
-   }
-   public String getIsbn() {
-    return isbn;
-   } 
-   public void setIsCheckedOut(boolean isCheckedOut){
-    this.isCheckedOut = isCheckedOut;
-   }
-   public boolean getIsCheckedOut() {
-    return isCheckedOut;
-   }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsCheckedOut(boolean isCheckedOut) {
+        this.isCheckedOut = isCheckedOut;
+    }
+
+    public boolean getIsCheckedOut() {
+        return isCheckedOut;
+    }
 }
