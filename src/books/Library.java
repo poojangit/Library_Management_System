86e670475bef010eecd0531a62458de4c18c79f8
poojangit <poojang147@gmail.com>
2025-01-06 11:59:19 +0530

    [POOJA] Added : implemented a method to add books to the library

--- src/books/Library.java
+++ src/books/Library.java
@@ -1,7 +1,37 @@
 package books;
 
+
 public class Library {
-    public void addBook(Book book) {}
+     Book[] books;
+     int bookCount;
+
+     public Library(int maxCapacity){
+        books = new Book[maxCapacity];
+        bookCount = 0;
+     }
+    public void addBook(Book book) {
+        if (bookCount<books.length) {
+            books[bookCount] = book;
+            bookCount++;
+            System.out.println("Book added !!");
+            System.out.println();
+        }
+        else {
+            System.out.println("Library is full");
+        }
+    }
+    public void displayBooks(){
+        if(bookCount == 0){
+            System.out.println("No books available!");
+        } 
+        else{
+            for(int i=0; i<bookCount; i++){
+                books[i].displayDetails();
+               System.out.println("-------------------");
+            }
+            System.out.println();
+        }
+    }
     public void removeBook(String isbn){}
     public void checkoutBook(String isbn){}
     public void returnBook(String isbn) {}
--- src/books/LibraryMainClass.java
+++ src/books/LibraryMainClass.java
@@ -8,7 +8,24 @@ public class LibraryMainClass {
         System.out.println("Enter your name : ");
         String name = sc.nextLine();
         System.out.println("Hello " + name + " welcome to Library");
-        Library library = new Library();
+        Library library = new Library(3);
+        System.out.println("How many books you need to add? : ");
+        int numOfBooks = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Now you can add " + numOfBooks +" books to the library !! "  );
+        for(int i=0; i< numOfBooks; i++){
+            System.out.println("Add your " + (i+1) + "st book");
+            System.out.println("Enter the Book title: ");
+            String title = sc.nextLine();
+            System.out.println("Author name: ");
+            String author = sc.nextLine();
+            System.out.println("ISBN number: ");
+            String isbn = sc.nextLine();
+            Book book = new Book(title, author, isbn, false );
+            library.addBook(book);
+        }
+        System.out.println("These are the books that are there in our library");
+        library.displayBooks();
         sc.close();
     }
 }
